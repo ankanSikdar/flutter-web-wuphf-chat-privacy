27c3bd2ada998236a1921a0a746e115a2d0a3679
Ankan Sikdar <ankansikdar@gmail.com>
2021-08-20 16:01:52 +0530

    Added app icon and designed app bar

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Wuphf Chat Privacy Policy',
+      title: 'Privacy Policy',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primaryColor: Colors.amber.shade700,
@@ -26,8 +26,33 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 4.0,
+        toolbarHeight: 125.0,
+        title: Row(
+          children: [
+            Container(
+              height: 100.0,
+              width: 100.0,
+              padding: EdgeInsets.all(10.0),
+              color: Colors.white,
+              child: Image.asset('assets/images/app_icon.png'),
+            ),
+            SizedBox(height: 18.0),
+            Expanded(
+              child: Text(
+                'Wuphf Chat Privacy Policy',
+                style: TextStyle(fontSize: 36.0),
+              ),
+            ),
+          ],
+        ),
+      ),
       body: Center(
-        child: Text('Privacy Policy'),
+        child: Text(
+          'Privacy Policy',
+        ),
       ),
     );
   }
