22a35ec016584766fade6a0de45417e7a2eec92a
Ankan Sikdar <ankansikdar@gmail.com>
2021-08-20 16:59:21 +0530

    Added service providers

--- lib/components/components.dart
+++ lib/components/components.dart
@@ -2,3 +2,4 @@ export 'privacy.dart';
 export 'info_collected.dart';
 export 'log_data.dart';
 export 'cookies.dart';
+export 'service_providers.dart';
---
+++ lib/components/service_providers.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:wuphf_privacy/widgets/widgets.dart';
+
+class ServiceProvider extends StatelessWidget {
+  const ServiceProvider({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Header(text: 'Service Providers'),
+        Content(
+          text:
+              'I may employ third-party companies and individuals due to the following reasons:\n\n⬛ To facilitate our Service;\n\n⬛ To provide the Service on our behalf;\n\n⬛ To perform Service-related services; or\n\n⬛ To assist us in analyzing how our Service is used.\n\nI want to inform users of this Service that these third parties have access to your Personal Information. The reason is to perform the tasks assigned to them on our behalf. However, they are obligated not to disclose or use the information for any other purpose.',
+        ),
+      ],
+    );
+  }
+}
