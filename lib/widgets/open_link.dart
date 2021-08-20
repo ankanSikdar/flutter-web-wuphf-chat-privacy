4dab8d33b1c9465538e47fe382e80bfc848b892e
Ankan Sikdar <ankansikdar@gmail.com>
2021-08-20 16:41:27 +0530

    Created open link widget

---
+++ lib/widgets/open_link.dart
@@ -0,0 +1,31 @@
+import 'dart:js' as js;
+import 'package:flutter/material.dart';
+
+class OpenLink extends StatelessWidget {
+  final String text;
+  final String url;
+  const OpenLink({
+    Key key,
+    @required this.text,
+    @required this.url,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16.0),
+      child: InkWell(
+        child: Text(
+          text,
+          style: TextStyle(
+            fontSize: 18.0,
+            color: Colors.blue,
+          ),
+        ),
+        onTap: () {
+          js.context.callMethod('open', [url]);
+        },
+      ),
+    );
+  }
+}
--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -1,2 +1,3 @@
 export 'header.dart';
 export 'content.dart';
+export 'open_link.dart';
